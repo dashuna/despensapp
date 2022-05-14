543b784fcbc4ccf6cbd0c07db8c12a3d4417b856
Dasha <darya.lynnyk@juanxxiii.net>
2022-05-14 12:24:15 +0200

    inicio

---
+++ src/main/java/com/dasha/despensapp/DespensappApplication.java
@@ -0,0 +1,13 @@
+package com.dasha.despensapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespensappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespensappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasha/despensapp/DespensappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasha.despensapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespensappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
